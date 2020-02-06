1b96e734a7e021771992424a6800571ac5936650
Kalvin Lam <kkl038@ucsd.edu>
2020-02-05 22:18:41 -0800

    Made Lab 5 Changes

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$('a.friend-name h3').click(friendClick);
+}
+
+function friendClick(e) {
+	e.preventDefault();
+	var name = $(this).text();
+	$(this).text(anagrammedName(name));
 }
 
 function anagrammedName(name) {
